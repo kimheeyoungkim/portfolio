6d08236f168e60ee0d6f8a17e680dbccf77aaa75
kimheeyoungkim <137054517+kimheeyoungkim@users.noreply.github.com>
2023-08-07 12:30:40 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,54 @@
+$(function(){
+    $('.snsad01 a').click(function(){
+        $('.snsadp01').fadeIn();//.show()
+    });
+    $('.snsadp01 a').click(function(){
+        $('.snsadp01').fadeOut();//.hide()
+    });
+});
+
+$(function(){
+    $('.snsad02 a').click(function(){
+        $('.snsadp02').fadeIn();//.show()
+    });
+    $('.snsadp02 a').click(function(){
+        $('.snsadp02').fadeOut();//.hide()
+    });
+});
+
+$(function(){
+    $('.snsad03 a').click(function(){
+        $('.snsadp03').fadeIn();//.show()
+    });
+    $('.snsadp03 a').click(function(){
+        $('.snsadp03').fadeOut();//.hide()
+    });
+});
+
+$(function(){
+    $('.snsad04 a').click(function(){
+        $('.snsadp04').fadeIn();//.show()
+    });
+    $('.snsadp04 a').click(function(){
+        $('.snsadp04').fadeOut();//.hide()
+    });
+});
+
+$(function(){
+    $('.banner01 a').click(function(){
+        $('.bannerp01').fadeIn();//.show()
+    });
+    $('.bannerp01 a').click(function(){
+        $('.bannerp01').fadeOut();//.hide()
+    });
+});
+
+
+$(function(){
+    $('.banner02 a').click(function(){
+        $('.bannerp02').fadeIn();//.show()
+    });
+    $('.bannerp02 a').click(function(){
+        $('.bannerp02').fadeOut();//.hide()
+    });
+});
